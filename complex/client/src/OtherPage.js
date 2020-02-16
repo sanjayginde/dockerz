cc1ec67ea6a349c9a9f92ec9c6535179bc7ca48b
Sanjay Ginde <sanjayginde@gmail.com>
2020-02-16 17:41:22 -0500

    Multi-container application [Section 8]

---
+++ complex/client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import Reach from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return(
+    <div>
+      I'm some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
---
+++ complex/server/index.js
@@ -0,0 +1,69 @@
+const keys = require('./keys');
+
+// Express app setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Postgres client setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  host: keys.pgHost,
+  port: keys.pgPort,
+  database: keys.pgDatabase,
+  user: keys.pgUser,
+  password: keys.pgPassword
+});
+pgClient.on('error', () => console.log('Lost Postgress connection'));
+pgClient.query('CREATE TABLE IF NOT EXISTS values(number INT)')
+  .catch((error) => console.log(error));
+
+
+// Redis client setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+
+//Express route handlers
+app.get('/', (request, response) => {
+  response.send('Hi');
+});
+
+app.get('/values/all', async (request, response) => {
+  const values = await pgClient.query('SELECT * FROM values');
+
+  response.send(values.rows);
+});
+
+app.get('/values/current', async (request, response) => {
+  redistClient.hgetall('values', (err, values) => {
+    response.send(values);
+  });
+});
+
+app.post('/values', async (request, response) => {
+  const index = request.body.value;
+  if (parseInt(index) > 40) {
+    return response.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  response.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log('Listening...');
+});
