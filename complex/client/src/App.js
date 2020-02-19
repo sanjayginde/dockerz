836e2962dcad819fe94f676de66ac95fba257f76
Sanjay Ginde <sanjayginde@gmail.com>
2020-02-19 11:16:50 -0500

    Dockerized multiple services [Section 9]

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'reactor-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
 
@@ -11,9 +11,9 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Edit <code>src/App.js</code> and save to reload.</h1>
+          <h1 className="App-title">Fibonnaci app</h1>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other page</Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
